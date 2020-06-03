6af12cf30bae5c66f0b794d1ff3c38ea4972fb76
zhan182493 <49330628+zhan182493@users.noreply.github.com>
2020-06-03 10:36:16 +0800

    数据库文件

--- journal.sql
+++ journal.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50553
  File Encoding         : 65001
 
- Date: 26/04/2020 15:36:54
+ Date: 03/06/2020 10:08:19
 */
 
 SET NAMES utf8mb4;
@@ -33,7 +33,7 @@ CREATE TABLE `acate`  (
 -- ----------------------------
 -- Records of acate
 -- ----------------------------
-INSERT INTO `acate` VALUES (1, '自然科学', NULL, 0, 0);
+INSERT INTO `acate` VALUES (1, '自然科学', '自然科学', 0, 0);
 INSERT INTO `acate` VALUES (4, '人工智能', '人工智能简介', 0, 0);
 
 -- ----------------------------
@@ -64,21 +64,26 @@ CREATE TABLE `article`  (
   `create_time` int(10) NOT NULL COMMENT '添加时间',
   `update_time` int(10) NOT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 47 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = MyISAM AUTO_INCREMENT = 54 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of article
 -- ----------------------------
-INSERT INTO `article` VALUES (34, '测试文章204', '1245-2356(2020)2-0-5', 48, '李斯', 1, '测试', '测试文章204摘要', '[参考文献1],[参考文献2],[参考文献3],[参考文献4]', 'N', 'E', 26, 5, 2, 2, 5, 0, 0, 1, 1587626000, 1587614230, 1587625968);
-INSERT INTO `article` VALUES (35, '测试文章200', '1245-2356(2020)1-5-6', 48, '李斯', 1, '测试', '测试文章200摘要', '[参考文献1],[参考文献2],[参考文献3],[参考文献4],[参考文献5],', 'N', 'E', 23, 5, 2, 1, 6, 2, 7, 1, 1587625996, 1587614318, 1587614318);
-INSERT INTO `article` VALUES (41, '测试文章205', '1245-2356(2020)3-22-8', 48, '李斯', 1, '测试', '测试文章205摘要测试文章205摘要测试文章205摘要d', '[参考文献],[参考文献],[参考文献],[参考文献],[参考文献],[参考文献],', 'N', 'E', 27, 5, 2, 3, 8, 10, 4, 1, 1587708706, 1587624712, 1587708674);
+INSERT INTO `article` VALUES (34, '测试文章204', '1245-2356(2020)2-0-5', 48, '李斯', 1, '测试', '测试文章204摘要', '[参考文献1],[参考文献2],[参考文献3],[参考文献4]', 'N', 'E', 26, 5, 2, 2, 5, 1, 0, 1, 1587626000, 1587614230, 1587625968);
+INSERT INTO `article` VALUES (35, '测试文章200', '1245-2356(2020)1-5-6', 48, '李斯', 1, '测试', '测试文章200摘要', '[参考文献1],[参考文献2],[参考文献3],[参考文献4],[参考文献5],', 'N', 'E', 23, 5, 2, 1, 6, 3, 7, 1, 1587625996, 1587614318, 1587614318);
+INSERT INTO `article` VALUES (41, '测试文章205', '1245-2356(2020)3-22-8', 48, '李斯', 1, '测试', '测试文章205摘要测试文章205摘要测试文章205摘要d', '[参考文献],[参考文献],[参考文献],[参考文献],[参考文献],[参考文献],', 'N', 'E', 27, 5, 2, 3, 8, 11, 5, 1, 1590671810, 1587624712, 1587708674);
 INSERT INTO `article` VALUES (40, '测试文章203', '1245-2356(2020)2-18-4', 48, '李斯', 1, '测试，文章203', '测试文章203摘要', '[参考文献],[参考文献],[参考文献],[参考文献],[参考文献],', 'N', 'E', 25, 5, 2, 2, 4, 0, 0, 1, 1587626000, 1587624670, 1587625935);
 INSERT INTO `article` VALUES (39, '测试文章201', '1245-2356(2020)1-11-7', 48, '李斯', 1, '测试文章,201', '测试文章201摘要', '[参考文献],[参考文献],[参考文献],[参考文献],[参考文献],', 'N', 'E', 24, 5, 2, 1, 7, 1, 1, 1, 1587625996, 1587624637, 1587625486);
 INSERT INTO `article` VALUES (42, '地外文明', '1245-2356(2020)3-30-4', 54, '李斯', 1, '地外文明', '地外文明摘要地外文明摘要地外文明摘要地外文明摘要地外文明摘要地外文明摘要地外文明摘要地外文明摘要地外文明摘要地外文明摘要地外文明摘要地外文明摘要', '[参考文献],[参考文献],[参考文献],[参考文献],[参考文献],[参考文献],[参考文献],[参考文献]', 'N', 'E', 31, 5, 1, 1, 4, 0, 0, 1, 1555817792, 1555731392, 1555731392);
 INSERT INTO `article` VALUES (43, 'RPA会计机器人', '1245-2356(2020)1-34-5', 54, '李斯', 4, 'RPA，会计机器人', 'RPA会计机器人摘要RPA会计机器人摘要RPA会计机器人摘要RPA会计机器人摘要RPA会计机器人摘要RPA会计机器人摘要RPA会计机器人摘要', '[参考文献],[参考文献],[参考文献],[参考文献],[参考文献],[参考文献],[参考文献],[参考文献]', 'N', 'E', 30, 5, 1, 1, 5, 0, 0, 1, 1555817792, 1555731392, 1555731392);
 INSERT INTO `article` VALUES (44, '海底微生物', '1245-2356(2020)2-39-4', 54, '李斯', 1, '海底，微生物', '海底微生物摘要海底微生物摘要海底微生物摘要海底微生物摘要海底微生物摘要海底微生物摘要海底微生物摘要海底微生物摘要', '[参考文献],[参考文献],[参考文献],[参考文献],[参考文献],[参考文献],[参考文献],[参考文献]', 'N', 'E', 32, 5, 1, 2, 4, 0, 0, 1, 1558409792, 1558323392, 1558323392);
 INSERT INTO `article` VALUES (45, '人工智能对当今社会影响', '1245-2356(2020)3-43-6', 54, '李斯', 4, '人工智能', '人工智能对当今社会影响摘要人工智能对当今社会影响摘要人工智能对当今社会影响摘要人工智能对当今社会影响摘要', '[参考文献],[参考文献],[参考文献],[参考文献],[参考文献],[参考文献],[参考文献],[参考文献],[参考文献]', 'N', 'E', 33, 5, 1, 3, 6, 0, 0, 1, 1561534289, 1561001792, 1561001792);
-INSERT INTO `article` VALUES (46, 'fdsa', '1245-2356(2020)3-49-4', 48, '李斯', 1, 'fdsa', 'fdsa摘要fdsa摘要fdsa摘要fdsa摘要fdsa摘要fdsa摘要fdsa摘要', '[参考文献],[参考文献],[参考文献],[参考文献],[参考文献],[参考文献],[参考文献]\r\n', 'N', 'E', 28, 5, 2, 3, 4, 1, 1, 1, 1587886154, 1587886154, 1587886154);
+INSERT INTO `article` VALUES (46, 'fdsa', '1245-2356(2020)3-49-4', 48, '李斯', 1, 'fdsa', 'fdsa摘要fdsa摘要fdsa摘要fdsa摘要fdsa摘要fdsa摘要fdsa摘要', '[参考文献],[参考文献],[参考文献],[参考文献],[参考文献],[参考文献],[参考文献]\r\n', 'N', 'E', 28, 5, 2, 3, 4, 1, 1, 1, 1590672342, 1587886154, 1587886154);
+INSERT INTO `article` VALUES (53, '测试文章502', '1245-2356(2020)3-75-6', 48, '李斯', 1, '测试文章', '测试文章502摘要测试文章502摘要测试文章502摘要测试文章502摘要测试文章502摘要', '参考文献参考文献参考文献参考文献参考文献参考文献参考文献参考文献', 'N', 'E', 35, 5, 2, 3, 6, 1, 2, 1, 1590671693, 1590651644, 1590651644);
+INSERT INTO `article` VALUES (49, '人工智能走进生活', '1245-2356(2020)3-57-5', 55, '李斯', 4, '人工智能', '人工智能走进生活摘要人工智能走进生活摘要人工智能走进生活摘要人工智能走进生活摘要人工智能走进生活摘要', '参考文献 参考文献 参考文献 参考文献 参考文献 参考文献 参考文献 参考文献 参考文献 参考文献 ', 'N', 'E', 36, 5, 2, 3, 5, 1, 1, 1, 1590672335, 1590650603, 1590650603);
+INSERT INTO `article` VALUES (50, '浅析人工智能对当今社会的影响', '1245-2356(2020)3-62-6', 55, '李斯', 4, '人工智能', '浅析人工智能对当今社会的影响摘要浅析人工智能对当今社会的影响摘要浅析人工智能对当今社会的影响摘要', '参考文献参考文献参考文献参考文献参考文献参考文献参考文献', 'N', 'E', 37, 5, 2, 3, 6, 0, 0, 1, 1590671829, 1590650661, 1590650661);
+INSERT INTO `article` VALUES (51, '人工智能基础与进阶教学', '1245-2356(2020)3-68-7', 55, '李斯', 4, '人工智能，教学', '人工智能基础与进阶教学摘要人工智能基础与进阶教学摘要人工智能基础与进阶教学摘要人工智能基础与进阶教学摘要人工智能基础与进阶教学摘要', '参考文献参考文献参考文献参考文献参考文献参考文献参考文献', 'N', 'E', 38, 5, 2, 3, 7, 0, 0, 1, 1590672320, 1590650706, 1590650706);
+INSERT INTO `article` VALUES (52, '人工智能与python', '1245-2356(2020)3-75-4', 55, '李斯', 4, '人工智能，python', '人工智能与python摘要人工智能与python摘要人工智能与python摘要人工智能与python摘要人工智能与python摘要', '参考文献参考文献参考文献参考文献参考文献参考文献参考文献', 'N', 'E', 39, 5, 2, 3, 4, 0, 0, 1, 1590672339, 1590650742, 1590650742);
 
 -- ----------------------------
 -- Table structure for auth_group
@@ -94,11 +99,11 @@ CREATE TABLE `auth_group`  (
 -- ----------------------------
 -- Records of auth_group
 -- ----------------------------
-INSERT INTO `auth_group` VALUES (1, '主编', '7,9,10,11,151,17,153,19,121,122,128,130,131,132,157,135,136,137,138,139,140,156,158,141,154,143,144,145,146,155,147,148,149,168,169,170,171,172');
+INSERT INTO `auth_group` VALUES (1, '主编', '7,9,10,11,151,17,153,19,121,122,128,130,131,132,157,135,136,137,138,139,140,156,158,173,141,154,143,144,145,146,155,147,148,149,168,169,170,171,172');
 INSERT INTO `auth_group` VALUES (2, '编辑', '7,9,10,11,151,17,153,19,121,122,128,157,135,156,158,141,154,146,155,147,148,149,168,169,170,171,172');
 INSERT INTO `auth_group` VALUES (3, '专家', '');
 INSERT INTO `auth_group` VALUES (4, '作者', NULL);
-INSERT INTO `auth_group` VALUES (13, '管理员', '7,9,10,11,151,8,160,164,163,162,161,12,14,15,16,152,123,124,125,126,127,17,153,19,121,122,128,130,131,132,133,134,157,135,136,137,138,139,140,156,158,141,154,143,144,145,146,155,147,148,149,168,169,170,171,172');
+INSERT INTO `auth_group` VALUES (13, '管理员', '7,9,10,11,151,8,160,164,163,162,161,12,14,15,16,152,123,124,125,126,127,17,153,19,121,122,128,130,131,132,133,134,157,135,136,137,138,139,140,156,158,173,141,154,143,144,145,146,155,147,148,149,168,169,170,171,172');
 INSERT INTO `auth_group` VALUES (14, '助手', '7,151,17,153,128,131,132,133,134,157,135,156,158,141,154,146,155,159');
 
 -- ----------------------------
@@ -126,6 +131,9 @@ INSERT INTO `auth_group_access` VALUES (50, 13);
 INSERT INTO `auth_group_access` VALUES (51, 3);
 INSERT INTO `auth_group_access` VALUES (52, 3);
 INSERT INTO `auth_group_access` VALUES (53, 3);
+INSERT INTO `auth_group_access` VALUES (54, 4);
+INSERT INTO `auth_group_access` VALUES (55, 4);
+INSERT INTO `auth_group_access` VALUES (56, 4);
 
 -- ----------------------------
 -- Table structure for auth_rule
@@ -137,7 +145,7 @@ CREATE TABLE `auth_rule`  (
   `name` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT ' 权限名',
   `pid` mediumint(9) UNSIGNED NOT NULL COMMENT ' 上级id',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 173 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Fixed;
+) ENGINE = MyISAM AUTO_INCREMENT = 174 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Fixed;
 
 -- ----------------------------
 -- Records of auth_rule
@@ -198,6 +206,7 @@ INSERT INTO `auth_rule` VALUES (169, 'News/lst', '动态列表', 168);
 INSERT INTO `auth_rule` VALUES (170, 'News/add', '添加动态', 168);
 INSERT INTO `auth_rule` VALUES (171, 'News/edit', '编辑动态', 168);
 INSERT INTO `auth_rule` VALUES (172, 'News/del', '删除动态', 168);
+INSERT INTO `auth_rule` VALUES (173, 'Journal/onefabu', '单个发布', 135);
 
 -- ----------------------------
 -- Table structure for draft
@@ -221,7 +230,7 @@ CREATE TABLE `draft`  (
   `create_time` int(20) NOT NULL COMMENT '投稿时间',
   `update_time` int(20) NOT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 34 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = MyISAM AUTO_INCREMENT = 42 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of draft
@@ -234,8 +243,15 @@ INSERT INTO `draft` VALUES (26, 48, '测试稿件204', '/uploads/draft/测试稿
 INSERT INTO `draft` VALUES (25, 48, '测试稿件203', '/uploads/draft/测试稿件203-詹德秋.doc', NULL, 1, 0, 1, 52, '', 1, 1, 1, 1587626000, 1587459616, 1587459616);
 INSERT INTO `draft` VALUES (24, 48, '测试稿件201', '/uploads/draft/测试稿件201-詹德秋.doc', NULL, 1, 0, 1, 53, '', 1, 1, 1, 1587625996, 1587458371, 1587458371);
 INSERT INTO `draft` VALUES (23, 48, '测试稿件200', '/uploads/draft/测试稿件200-詹德秋.doc', NULL, 1, 0, 1, 53, '', 1, 1, 1, 1587625996, 1587458348, 1587458348);
-INSERT INTO `draft` VALUES (27, 48, '测试稿件205', '/uploads/draft/测试稿件205-詹德秋.doc', NULL, 1, 0, 1, 53, '', 1, 1, 1, 1587708706, 1587459684, 1587459684);
-INSERT INTO `draft` VALUES (28, 48, 'fdsa', '/uploads/draft/fdsa-詹德秋.doc', NULL, 1, 0, 1, 52, '', 1, 1, 1, 1587886379, 1587530299, 1587530299);
+INSERT INTO `draft` VALUES (27, 48, '测试稿件205', '/uploads/draft/测试稿件205-詹德秋.doc', NULL, 1, 0, 1, 53, '', 1, 1, 1, 1590671810, 1587459684, 1587459684);
+INSERT INTO `draft` VALUES (28, 48, 'fdsa', '/uploads/draft/fdsa-詹德秋.doc', NULL, 1, 0, 1, 52, '', 1, 1, 1, 1590672342, 1587530299, 1587530299);
+INSERT INTO `draft` VALUES (34, 48, '测试稿件123', '/uploads/draft/测试稿件123-詹德秋.pdf', NULL, 1, 0, 1, 53, '需要改进', 0, 0, 0, 0, 1590587765, 1590587765);
+INSERT INTO `draft` VALUES (35, 48, '测试稿件502', '/uploads/draft/测试稿件502-詹德秋.pdf', NULL, 1, 0, 1, 52, '', 1, 1, 1, 1590671693, 1590648277, 1590648277);
+INSERT INTO `draft` VALUES (36, 55, '人工智能走进生活', '/uploads/draft/人工智能走进生活-徐东京.pdf', NULL, 4, 0, 1, 51, '', 1, 1, 1, 1590672335, 1590649886, 1590649886);
+INSERT INTO `draft` VALUES (37, 55, '浅析人工智能对当今社会的影响', '/uploads/draft/浅析人工智能对当今社会的影响-徐东京.pdf', NULL, 4, 0, 1, 51, '', 1, 1, 1, 1590671829, 1590649941, 1590649941);
+INSERT INTO `draft` VALUES (38, 55, '人工智能基础与进阶教学', '/uploads/draft/人工智能基础与进阶教学-徐东京.pdf', NULL, 4, 0, 1, 51, '', 1, 1, 1, 1590672320, 1590649977, 1590649977);
+INSERT INTO `draft` VALUES (39, 55, '人工智能之python', '/uploads/draft/人工智能之python-徐东京.pdf', NULL, 4, 0, 1, 6, '', 1, 1, 1, 1590672339, 1590650078, 1590650078);
+INSERT INTO `draft` VALUES (41, 48, '测试稿件456', '/uploads/draft/测试稿件456-詹德秋.pdf', NULL, 1, 0, 0, 53, '', 0, 0, 0, 0, 1590823149, 1590823149);
 
 -- ----------------------------
 -- Table structure for journal
@@ -260,7 +276,7 @@ CREATE TABLE `journal`  (
 -- ----------------------------
 -- Records of journal
 -- ----------------------------
-INSERT INTO `journal` VALUES (5, '派斯学院学报', 39, '派斯学院学报', '/uploads/fengmian/20200415\\37f04b291f2b2dffbe01003d68df86b8.jpg', '月刊', '1245-2356', '1543/TD', 123, '', 1586930018, 1586930018);
+INSERT INTO `journal` VALUES (5, '派斯学院学报', 39, '派斯学院学报', '/uploads/fengmian/20200415\\37f04b291f2b2dffbe01003d68df86b8.jpg', '月刊', '1245-2356', '1543/TD', 123, '', 1548659128, 1586930018);
 
 -- ----------------------------
 -- Table structure for link
@@ -299,12 +315,12 @@ CREATE TABLE `news`  (
 -- ----------------------------
 -- Records of news
 -- ----------------------------
-INSERT INTO `news` VALUES (1, '动态1', '<p>新一期投稿通告：&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p><p>&nbsp;请在2020年4月10日前完成本期投稿</p>', 1586784747, 1586858143);
-INSERT INTO `news` VALUES (2, '动态2', '<p>&nbsp;&nbsp;&nbsp;&nbsp;近期将开展期刊建设会议&nbsp;&nbsp;&nbsp;&nbsp;</p><p>由编辑组人员开会商讨进一步加强期刊建设方案。</p>', 1586784747, 1586784803);
-INSERT INTO `news` VALUES (12, '动态3', '<p style=\"text-align: center;\">关于投稿须知条款更改：</p><ol class=\" list-paddingleft-2\" style=\"list-style-type: decimal;\"><li><p>投稿须在本期截止日前进行</p></li><li><p>投稿需以pdf\\wps\\doc文件投递</p></li><li><p>如未及时支付稿费请联系我们</p></li></ol>', 1586784747, 1586784789);
-INSERT INTO `news` VALUES (9, '动态4', '<p>&nbsp;&nbsp;&nbsp;&nbsp;今日起截至2020年4月20日，&nbsp;&nbsp;&nbsp;&nbsp;</p><p>开始征集关于“我劳动我光荣”主题文章，&nbsp;&nbsp;&nbsp;&nbsp;</p><p>请广大作者们积极投稿</p>', 1586784747, 1586784813);
-INSERT INTO `news` VALUES (7, '动态5', '<p>&nbsp; &nbsp; &nbsp; 为倡导劳动光荣，我们将举行劳动节写作大赛，获得名次有丰厚奖品，大家快快快参与bb</p>', 1586784747, 1586784776);
-INSERT INTO `news` VALUES (14, '动态6', '<p>测试公告测试公告测试公告测试公告测试公告测试公告测试公告</p><p>测试公告测试公告</p><p><br/></p>', 1586784837, 1586784837);
+INSERT INTO `news` VALUES (1, '新一期投稿', '<p>新一期投稿通告：&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p><p>&nbsp;请在2020年6月10日前完成本期投稿。</p>', 1586784747, 1590806964);
+INSERT INTO `news` VALUES (2, '近期将开展期刊建设会议', '<p>&nbsp;&nbsp;&nbsp;&nbsp;近期将开展期刊建设会议&nbsp;&nbsp;&nbsp;&nbsp;</p><p>由编辑组人员开会商讨进一步加强期刊建设方案。。</p>', 1586784747, 1590806982);
+INSERT INTO `news` VALUES (12, '关于投稿须知条款更改', '<p style=\"text-align: center;\">关于投稿须知条款更改：</p><ol class=\" list-paddingleft-2\" style=\"list-style-type: decimal;\"><li><p>投稿须在本期截止日前进行</p></li><li><p>投稿需以pdf\\wps\\doc文件投递</p></li><li><p>如未及时支付稿费请联系我们。</p></li></ol>', 1586784747, 1590806994);
+INSERT INTO `news` VALUES (9, '文章征集', '<p>&nbsp;&nbsp;&nbsp;&nbsp;今日起截至2020年4月20日，&nbsp;&nbsp;&nbsp;&nbsp;</p><p>开始征集关于“我劳动我光荣”主题文章，&nbsp;&nbsp;&nbsp;&nbsp;</p><p>请广大作者们积极投稿。</p>', 1586784747, 1590807031);
+INSERT INTO `news` VALUES (7, '劳动节写作大赛', '<p>&nbsp; &nbsp; &nbsp; 为倡导劳动光荣，我们将举行劳动节写作大赛，获得名次有丰厚奖品，大家快快快参与。</p>', 1586784747, 1590807043);
+INSERT INTO `news` VALUES (14, '测试动态', '<p>测试动态测试动态测试动态测试动态测试动态测试动态测试动态测试动态测试动态测试动态测试动态测试动态测试动态</p>', 1586784837, 1590807066);
 
 -- ----------------------------
 -- Table structure for notice
@@ -327,8 +343,8 @@ INSERT INTO `notice` VALUES (2, '近期将开展期刊建设会议1', '<p>&nbsp;
 INSERT INTO `notice` VALUES (12, '关于投稿须知条款更改1', '<p style=\"text-align: center;\">关于投稿须知条款更改：</p><ol class=\" list-paddingleft-2\" style=\"list-style-type: decimal;\"><li><p>投稿须在本期截止日前进行</p></li><li><p>投稿需以pdf\\wps\\doc文件投递</p></li><li><p>如未及时支付稿费请联系我们</p></li></ol>', 1586784747, 1586784789);
 INSERT INTO `notice` VALUES (9, '征文啦1', '<p>&nbsp;&nbsp;&nbsp;&nbsp;今日起截至2020年4月20日，&nbsp;&nbsp;&nbsp;&nbsp;</p><p>开始征集关于“我劳动我光荣”主题文章，&nbsp;&nbsp;&nbsp;&nbsp;</p><p>请广大作者们积极投稿</p>', 1586784747, 1586784813);
 INSERT INTO `notice` VALUES (7, '5*1劳动节写作大赛火热进行中', '<p>&nbsp; &nbsp; &nbsp; 为倡导劳动光荣，我们将举行劳动节写作大赛，获得名次有丰厚奖品，大家快快快参与bb</p>', 1586784747, 1586784776);
-INSERT INTO `notice` VALUES (14, '测试公告', '<p>测试公告测试公告测试公告测试公告测试公告测试公告测试公告</p><p>测试公告测试公告</p><p><br/></p>', 1586784837, 1586784837);
-INSERT INTO `notice` VALUES (15, '测试公告', '<p>测试公告测试公告测试公告测试公告测试公告</p><p>测试公告测试公告测试公告测试公告</p><p>测试公告测试公告测试公告测试公告测试公告</p><p>测试公告测试公告测试公告测试公告</p><p>测试公告测试公告测试公告测试公告测试公告</p>', 1587291526, 1587291526);
+INSERT INTO `notice` VALUES (14, '测试公告1', '<p>测试公告1测试公告1测试公告1测试公告1测试公告1测试公告1测试公告1测试公告1测试公告1</p><p><br/></p>', 1586784837, 1590807093);
+INSERT INTO `notice` VALUES (15, '测试公告2', '<p>测试公告2测试公告测试公告测试公告测试公告</p><p>测试公告2测试公告测试公告测试公告</p><p>测试公告2测试公告测试公告测试公告测试公告</p><p>测试公告2测试公告测试公告测试公告</p><p>测试公告2测试公告测试公告测试公告测试公告</p>', 1587291526, 1590807104);
 
 -- ----------------------------
 -- Table structure for user
@@ -357,7 +373,7 @@ CREATE TABLE `user`  (
   `create_time` int(11) NOT NULL COMMENT '添加时间',
   `update_time` int(11) NOT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 55 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = MyISAM AUTO_INCREMENT = 57 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
@@ -371,5 +387,7 @@ INSERT INTO `user` VALUES (54, '何洋', 'Young', 'b872741bc7aadd26dc3154e7e1cae
 INSERT INTO `user` VALUES (51, '王五', 'wangwu', 'f5890f526a5c8bdbc37fbc2a7fdebfea', 'wangwu@gmail.com', '18945122356', '', 1, 0, '', '', '', '', '', '', '', '', '', 4, 0, 0);
 INSERT INTO `user` VALUES (52, '张三', 'zhangsan', 'd090871b5cddf8124a2a0434c0c716cf', 'zhangsan@gmail.com', '13545124578', '', 1, 0, '', '', '', '', '', '', '', '', '', 1, 0, 0);
 INSERT INTO `user` VALUES (53, '李二', 'lier', '6e476c93d92eca495d6af0fc8e0aa0e9', 'lier@gmail.com', '18045562348', '', 1, 0, '', '', '', '', '', '', '', '', '', 1, 0, 0);
+INSERT INTO `user` VALUES (55, '徐东京', 'xudongjing', '7dfd7cf28c52ccbeaa65f98e89c379e1', 'xdj@qq.com', '15236547845', '500115199704154555', 1, 0, '新疆', '河南', '本科', '计算机', '无', '无', '网站开发', '寝室', '115', 0, 0, 1590649843);
+INSERT INTO `user` VALUES (56, '作者', 'zuozhe', '5cc6625158fc77c524693ed56422eb68', 'zuozhe@qq.com', '15233145568', '422828199710022951', 1, 22, '湖北鹤峰', '湖北鹤峰', '本科', '计算机', '华龙网', 'PHP程序员', '网站开发', '宿舍', '115', 0, 1590799642, 1590799642);
 
 SET FOREIGN_KEY_CHECKS = 1;
